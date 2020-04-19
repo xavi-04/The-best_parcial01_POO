d736c508871c9b75f8448b30df1bcd28c3364e62
xavi-04 <chavidelcid@gmail.com>
2020-04-18 19:04:09 -0600

    Add: Modify class

--- The-best_parcial01_POO/src/com/XAQC/x00048219/CalculadoraImpuestos.java
+++ The-best_parcial01_POO/src/com/XAQC/x00048219/CalculadoraImpuestos.java
@@ -4,33 +4,42 @@ package com.XAQC.x00048219;
 
     private static double totalRenta, totalISSS, totalAFP;
 
-    private CalculadoraImpuestos(){
+    private CalculadoraImpuestos(){}
 
-    }
+
     public static double calcularPago(Empleado){
-
-        if (){
-            totalRenta = (0.1 * Salario);
-
-            pagoEmpleado = (Salario - totalRenta);
-        }else {
-            totalAFP = (0.0625 * Salario);
-            totalISSS = (0.03 * Salario);
-
-            restante = (Salario - totalISSS- totalAFP);
-
-            totalRenta = 0;
-            totalRenta = (0.1 * (restante - 472) + 17.67);
-            totalRenta = (0.2 * (restante - 895.24) + 60);
-            totalRenta = (0.3 * (restante- 2038.10) + 288.57);
+        double restante, ISSS, AFP, renta, rentame = 0, pagoEmpleado;
 
 
-            pagoEmpleado = (restante - totalRenta);
+            if(Empleado instanceof PlazaFija){
+                AFP = ((PlazaFija)Empleado).Salario * 0.0625;
+                ISSS = ((PlazaFija)Empleado).Salario * 0.03;
+
+                restante = (((PlazaFija)Empleado).Salario - AFP -ISSS);
+                if( ((PlazaFija)Empleado)).Salario >=0.01 && ((PlazaFija)Empleado).Salario < 472){
+                rentame = 0;
+                }
+                if( ((PlazaFija)Empleado)).Salario >=472 && ((PlazaFija)Empleado).Salario < 895.24){
+                    rentame = (0.1 * (restante - 472) + 17.67);
+                }
+                if( ((PlazaFija)Empleado)).Salario >=895.24 && ((PlazaFija)Empleado).Salario < 2038.10){
+                    rentame = (0.2 * (restante - 895.24) + 60);
+                }
+                if( ((PlazaFija)Empleado)).Salario >= 2038.10){
+                    rentame = (0.3 * (restante- 2038.10) + 288.57);
+                }
+
+                pagoEmpleado = (restante - rentame);
+
         }
+            else{
+                if (Empleado instanceof ServicioProfesional){
+                    renta = (0.1 * Salario);
 
-
-
-   }
+                    pagoEmpleado = (Salario - renta);
+                }
+            }
+    }
 
     public static String mostrarTotales() {
 
@@ -42,3 +51,20 @@ package com.XAQC.x00048219;
         //Rango B: [$ 472.00, $ 895.24]
         //Rango C: [$ 895.24, $ 2,038.11]
         //Rango D: [$ 2,038.11 o más]
+
+        /*totalRenta = (0.1 * Salario);
+
+        pagoEmpleado = (Salario - totalRenta);
+
+        totalAFP = (0.0625 * Salario);
+        totalISSS = (0.03 * Salario);
+
+        restante = (Salario - totalISSS- totalAFP);
+
+        totalRenta = 0;
+        totalRenta = (0.1 * (restante - 472) + 17.67);
+        totalRenta = (0.2 * (restante - 895.24) + 60);
+        totalRenta = (0.3 * (restante- 2038.10) + 288.57);
+
+
+        pagoEmpleado = (restante - totalRenta);*/
